f8d742947039004db8b6e5648d09bbdd588603cf
ysnztrk021 <yamoz1030@gmail.com>
2022-05-06 13:32:54 +0200

    pin

--- src/Controller/PinController.php
+++ src/Controller/PinController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Pin;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +14,9 @@ class PinController extends AbstractController
      */
     public function index(): Response
     {
+        $pin = new Pin;
+        dump($pin);
+        // die;
         return $this->render('pin/index.html.twig', [
             'controller_name' => 'PinController',
         ]);
