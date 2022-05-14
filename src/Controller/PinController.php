16eb461fb6538319e12f029a68d46cbeb23b2b0c
ysnztrk021 <yamoz1030@gmail.com>
2022-05-14 13:24:32 +0200

    changes

--- src/Controller/PinController.php
+++ src/Controller/PinController.php
@@ -7,6 +7,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\PinRepository;
+use App\Form\PinType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 class PinController extends AbstractController
 {
@@ -15,15 +18,7 @@ class PinController extends AbstractController
      */
     public function index(PinRepository $repo): Response
     {
-
-
-        // die;
         return $this->render('pin/index.html.twig', ['pins' => $repo->findAll()]);
-        // return $this->render('pin/index.html.twig', [
-        //     'controller_name' => 'PinController',
-        // ]);
-        // return new Response('Hello World');
-        // return $this->json(['message'=>'Hello World']);
     }
     /**
      * @Route("/pin/{id<[0-9]+>}", name="app_pin_show", methods="GET")
@@ -31,5 +26,20 @@ class PinController extends AbstractController
     public function show(Pin $pin): Response
     {
         return $this->render('pin/show.html.twig', compact('pin'));
+    }
+    /**
+     * @Route("/pin/create", name="app_pin_create", methods= {"GET","POST"})
+     */
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        $pin = new Pin;
+        $form = $this->createForm(PinType::class, $pin);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($pin);
+            $em->flush();
+            return $this->redirectToRoute('app_home');
+        }
+        return $this->render('pin/create.html.twig', ['monForm' => $form->createView()]);
     }
 }
